2c714444854991c32e055452e4f76232c219b7f7
akey96 <akeymy4@gmail.com>
2022-05-03 17:51:10 -0400

    Feat: Configured django manager for profile model (#22)

--- service/profiles/admin.py
+++ service/profiles/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from profiles.models import Profile
 
-admin.site.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'rol', 'email',)
+    ordering = ('first_name', 'last_name', 'rol', )
+    search_fields = ('first_name', 'last_name', 'rol', 'email',)
+    list_display_links = ('first_name', )
+    list_filter = ('rol',)
+    list_per_page = 10
+
+admin.site.register(Profile, ProfileAdmin)
